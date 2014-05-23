7c7b9483f6f68978e9fb7ac9e05452f3f7aa076d
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-05-23 07:52:40 -0400

    cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_action :require_signed_in!
-
   helper_method :require_signed_in!, :current_user
-
 
   private
   def current_user
