ba04214ae289ef55bc5f589026d7841900462a8a
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-05-25 14:18:05 -0400

    makes affiliation

--- app/models/affiliation.rb
+++ app/models/affiliation.rb
@@ -1,8 +1,23 @@
 class Affiliation < ActiveRecord::Base
 
+	ROLES = {
+		1 => :debater,
+		2 => :judge
+	}
+
+	ROLE_IDS = ROLES.invert
+
 	belongs_to :school, inverse_of: :affiliations, counter_cache: true 
 	belongs_to :affiliatable, inverse_of: :affiliations, polymorphic: true, counter_cache: true
-	
 
+	validates :role_id, inclusion: { in: ROLES.keys }
+	validates :affiliatable, :school, presence: true
+
+	scope :role, ->(role_name) { where(role_id: ROLE_IDS[role_name] )}
+
+
+	def role_name
+		ROLES[self.role_id]
+	end 
 
 end
--- db/migrate/20140523022053_create_judges.rb
+++ db/migrate/20140523022053_create_judges.rb
@@ -2,6 +2,7 @@ class CreateJudges < ActiveRecord::Migration
   def change
     create_table :judges do |t|
     	t.string :name
+    	t.integer :rank
 
       t.timestamps
     end
