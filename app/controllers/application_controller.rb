62048df5d07a6c2f079ad36e971951d8d2995e3d
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-05-30 08:48:23 -0400

    finishes session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,28 +3,28 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :require_signed_in!
-  helper_method :require_signed_in!, :current_user
+  helper_method :require_signed_in!, :current_user, :require_signed_out!
 
   private
   def current_user
-  	return nil unless session[:token]
-  	@current_user ||= User.find_by(token: session[:token])
+    return nil unless session[:token]
+    @current_user ||= User.find_by(token: session[:token])
   end
 
   def signed_in?
-  	!!current_user
+    !!current_user
   end
 
   def sign_in(user)
-  	@current_user = user
-  	session[:token] = user.reset_token!
+    @current_user = user
+    session[:token] = user.reset_token!
   end
 
   def require_signed_in!
-  	redirect_to new_session_url unless signed_in?
+    redirect_to new_session_url unless signed_in?
   end
 
-  def require_signed_out
-  	redirect_to user_url(current_user) if signed_in?
+  def require_signed_out!
+    redirect_to user_url(current_user) if signed_in?
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,28 @@
 class SessionsController < ApplicationController
+  before_filter :require_signed_in!, only: [:destroy]
+  before_filter :require_signed_out!, only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    if @user
+      login(@user)
+      flash[:notices] = ["Welcome to GregTab, #{@user.username}!"]
+      redirect_to root_url
+    else
+      flash[:errors] = ["Invalid Username or Password."]
+      @user = User.new
+      render :new
+    end
+  end
+
+  def destroy
+    logout!
+    flash[:notices] = ["Logged out sucessfully!"]
+    redirect_to new_session_url
+  end
+
 end
