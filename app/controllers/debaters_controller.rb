fd4b31e844e0465d2217db39717f9322eafc24bb
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-06-01 22:12:00 -0400

    adds debaters controller

--- app/controllers/debaters_controller.rb
+++ app/controllers/debaters_controller.rb
@@ -1,2 +1,27 @@
 class DebatersController < ApplicationController
+
+  def show
+    @debater = Debater.find(params[:id])
+  end
+
+  def new
+    @debater = Debater.new
+  end
+
+  def create
+    @debater = Debater.new(debater_params)
+    #I need to add a new thing here for schools
+    if @debater.save
+      flash[:notices] = ["You made a new debater"]
+      redirect_to debaters_url
+    else
+      flash[:errors] = @debater.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+  def debater_params
+    params.require(:debater).permit(:name)
+  end
 end
